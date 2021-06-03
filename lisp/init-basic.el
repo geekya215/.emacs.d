5a3bf2c9e5947a3520a4972c08e5eae3eb12fca4
Lin Yang <geekya215@gmail.com>
2021-06-03 20:45:06 +0800

    config relative number

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -7,6 +7,10 @@
 ;; disable scroll bar
 (scroll-bar-mode -1)
 
+(setq display-line-numbers-type 'relative) 
+
+(global-display-line-numbers-mode t)
+
 ;; do not use backup file
 (setq make-backup-files nil)
 
